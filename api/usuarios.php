4a92ddadd3f5adecf0eb7580f2b56b0e16127173
braulio flores <bfloresl1400@alumno.ipn.mx>
2021-11-23 09:33:08 -0600

    backend terminado

--- api/usuarios.php
+++ api/usuarios.php
@@ -2,15 +2,19 @@
     // echo $_SERVER["REQUEST_METHOD"];
     // NOS DEVULVE EL METODO POR EL QUE SE ESTA PIDIENDO LA PAGINA
     
-    header("Content-Type: application/json");
+    header("Content-Type: application/json"); //PARA INDICAR QUE VAMOS A DEVOLVER JSON
     // echo 'Informacion '.file_get_contents('php://input'); //ESTO NOS PERMITE VER LA INFORMACION QUE ESTA MANDANDO EL USUARIO
+    include_once "../clases/class-usuarios.php";
     switch ($_SERVER["REQUEST_METHOD"]) {
         case 'POST':
             $_POST = json_decode(file_get_contents('php://input'),true);
             // POST TENDRIA QUE SER UN ARREGLO ASOCIATIVO, ENTONCES TENEMOS QUE DESENCODE EL JSON Y PASARLE TRUE PARA QUE SEA ASOCIATIVO
             // POR QUE FILE GET CONTENTS NOS DEVUELVE UN JSON
             // ENTONCES LO ESTAMOS GUARDANDO EN UN ARREGLO ASOCIATIVO QUE SE LLAMA $_POST
-            $resultado["mensaje"] = "Guardar usuario ".json_encode($_POST);
+            $usuarioP = new Usuario($_POST["nombre"],$_POST["apellido"],$_POST["fechaNacimiento"],$_POST["pais"]);
+            echo $usuarioP->toString();
+            $usuarioP->guardarUsuario();
+            $resultado["mensaje"] = "Usuario Guardado ".json_encode($_POST);
             echo json_encode($resultado);
             
         break;
@@ -18,10 +22,14 @@
             if (isset($_GET['id'])) {
                 $resultado["mensaje"] = "Obtener usuario ".$_GET["id"];
                 echo json_encode($resultado);
+                echo "--------";
+                Usuario::obtenerUsuario($_GET['id']);
             }
             else{
                 $resultado["mensaje"] = "Retornar todos los usuarios";
                 echo json_encode($resultado);
+                echo "----";
+                echo Usuario::obtenerUsuarios();
             }
         break;
         case 'PUT':
@@ -29,10 +37,13 @@
             $resultado["mensaje"] = "Altualizar usuario con el id ".$_GET['id'].
                                     " Informacion a actualizar: ". json_encode($_PUT);
             echo json_encode($resultado);
+            echo "-------------";
+            Usuario::actualizarUsuario($_GET['id'],$_PUT["nombre"],$_PUT["apellido"],$_PUT["fechaNacimiento"],$_PUT["pais"]);
         break;
         case 'DELETE':
             $resultado["mensaje"] = "Eliminar usuario = ".$_GET['id'];
             echo json_encode($resultado);
+            Usuario::eliminarUsuario($_GET['id']);
         break;
     }
 ?>
--- clases/class-usuarios.php
+++ clases/class-usuarios.php
@@ -6,18 +6,68 @@
         private $fechaNacimiento;
         private $pais;        
 
+        public function __construct($nombre,$apellido,$fechaNacimiento,$pais){
+                $this->nombre = $nombre;
+                $this->apellido = $apellido;
+                $this->fechaNacimiento = $fechaNacimiento;
+                $this->pais = $pais;
+        }
+
+        public function toString(){
+                return "Mi nombre es ".$this->nombre;
+        }
         // CRUD
         public function guardarUsuario(){
-
+                $contenido = file_get_contents("../data/usuarios.json");
+                $usuarios = json_decode($contenido,true);
+                // CUANDO USAMOS UN CORCHETE VACIO NOS DA EL INDICE 
+                // DEL ULTIMO REGISTRO $usuarios[]
+                $usuarios[] = array(
+                        "nombre"=> $this->nombre,
+                        "apellido"=> $this->apellido,
+                        "fechaNacimiento"=> $this->fechaNacimiento,
+                        "pais"=> $this->pais
+                );
+                // Esto se hace para evitar lo siguiente
+                // usuarios["nombre"] = $this->nombre;
+                // usuarios["apellido"] = $this->apellido;
+                // etc...
+                // SE GENERA UN ARREGLO AOCIATIVO DE PHP 
+                $archivo = fopen("../data/usuarios.json","w"); //SEGUDO PARAMETRO ES EL METODO DE APERTURA W PARA WRITE 
+                fwrite($archivo,json_encode($usuarios));
+                fclose($archivo);
         }
-        public function obtenerUsuario(){
-
+        public static function obtenerUsuarios(){
+                $contenido = file_get_contents("../data/usuarios.json");
+                echo $contenido;
         }
-        public function actualizarUsuario(){
 
+        public static function obtenerUsuario($indice){
+                $contenido = file_get_contents("../data/usuarios.json");
+                $usuarios = json_decode($contenido,true);
+                echo json_encode($usuarios[$indice]);
         }
-        public function eliminarUsuario(){
-            
+
+        public static function actualizarUsuario($indice,$newNombre,$newApellido,$newFechaNacimiento,$newPais){
+                $contenido = file_get_contents("../data/usuarios.json");
+                $usuarios = json_decode($contenido,true);
+                $usuarios[$indice] = array(
+                        "nombre"=> $newNombre,
+                        "apellido"=> $newApellido,
+                        "fechaNacimiento"=> $newFechaNacimiento,
+                        "pais"=> $newPais
+                );
+                $archivo = fopen("../data/usuarios.json","w"); //SEGUDO PARAMETRO ES EL METODO DE APERTURA W PARA WRITE 
+                fwrite($archivo,json_encode($usuarios));
+                fclose($archivo);
+        }
+        public static function eliminarUsuario($indice){
+                $contenido = file_get_contents("../data/usuarios.json");
+                $usuarios = json_decode($contenido,true);
+                array_splice($usuarios,$indice,1);
+                $archivo = fopen("../data/usuarios.json","w"); //SEGUDO PARAMETRO ES EL METODO DE APERTURA W PARA WRITE 
+                fwrite($archivo,json_encode($usuarios));
+                fclose($archivo);
         }
 
         /**
